72d9215aa33e4fc4a11fe954c5c7c5d3164ca2d6
distanceoh <distanceoh@hotmail.com>
2024-07-17 18:40:32 -0500

    testy2 is working

--- testy2.py
+++ testy2.py
@@ -2,7 +2,7 @@ import pandas
 import pprint as p
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, Text, Float, DATE
 import csv
 
@@ -18,8 +18,31 @@ class Base(DeclarativeBase):
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-from main import Item
-from main import Group
+
+class Item(db.Model):
+    __tablename__ = "items"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=True)
+    average: Mapped[float] = mapped_column(Float, nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=True)
+    on_hand: Mapped[float] = mapped_column(Float, nullable=True)
+    needed: Mapped[float] = mapped_column(Float, nullable=True)
+    last_counted: Mapped[str] = mapped_column(String(250), nullable=True)
+    unit: Mapped[str] = mapped_column(String(250), nullable=True)
+    sort: Mapped[str] = mapped_column(String(250), nullable=True)
+    group_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("groups.id"))
+    # Create reference to the User object. The "posts" refers to the posts property in the User class.
+    group = relationship("Group", back_populates="items")
+
+
+class Group(db.Model):
+    __tablename__ = "groups"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    weeks: Mapped[float] = mapped_column(Float, nullable=False)
+    items = relationship("Item", back_populates="group")
+
 
 # class Item(db.Model):
 #     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -42,22 +65,23 @@ def create_records(name, price, location, average, on_hand, group, unit, sort):
     with app.app_context():
         old_book = db.session.execute(db.select(Item).where(Item.name == name)).scalar()
         selected_group = db.session.execute(db.select(Group).where(Group.name == group)).scalar()
+        print(selected_group.items)
         new_book = Item(name=name, price=price, location=location, average=average, on_hand=on_hand,
-                        group=selected_group, unit=unit, sort = sort)
+                        group=selected_group, unit=unit, sort=sort)
         if not old_book:
             db.session.add(new_book)
         else:
             pass
             # old_book.price = price
             # old_book.unit = unit
-
+        print(new_book.name)
         db.session.commit()
 
 
 if __name__ == '__main__':
     data = pandas.read_csv("static/data/testdata2.csv")
     data = data.to_dict(orient='records')
-    p.pprint(data)
+    # p.pprint(data)
     for row in data:
         create_records(name=row['Name'], price=row['price'], location=row['location'], average=row['average'],
                        on_hand=row['on_hand'], group=row['group'], unit=row['unit'], sort=row['sort'])
